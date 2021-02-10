3a8fea6ed54c55e3bbdbd579f05cd456a376ac7d
revovich <revovich@gmail.com>
2021-02-11 00:18:33 +0300

    v-[cloak,html,once,text,pre]

--- app.js
+++ app.js
@@ -1,38 +1,6 @@
-const App = {
-    data() {
-        return {
-            title: 'To-do',
-            placeholder: 'Add new item',
-            inputValue:'',
-            todoList: ['note 1', 'note 2', 'note 42']
-        }
-    },
-    methods: {
-        addNewNote() {
-            if (this.inputValue !== '') {
-                this.todoList.push(this.inputValue),
-                this.inputValue = ''  
-            }
-        },
-        toUpperCase(item) {
-            return item.toUpperCase()
-        },
-        removeNote(idx) {
-            this.todoList.splice(idx, 1)
-        }
-    },
-    computed: {
-        doubleCount() {
-            return this.todoList.length * 2
-        }
-    },
-    watch: {
-        inputValue(value) {
-            console.log(value)
-        }
-    }
-}
-
-const app = Vue.createApp(App)
-
-app.mount('#app')
+Vue.createApp({
+    data: () => ({
+        title: 'I am the Title)',
+        myHtml: '<h1>My HTML title</h1>'
+    })
+}).mount('#app')
