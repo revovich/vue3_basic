d466deb9e1e1690d6ea51b69b7a3091e53947a22
revovich <revovich@gmail.com>
2021-02-17 01:52:54 +0300

    v-for with Object

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 Vue.createApp({
     data: () => ({
         title: 'I am the Title)',
-        myHtml: '<h1>My HTML title</h1>'
+        myHtml: '<h1>My HTML title</h1>',
+        person: {
+            firstName: 'Daniel',
+            secondName: 'Haboosh',
+            age: 38
+        }
     })
 }).mount('#app')
